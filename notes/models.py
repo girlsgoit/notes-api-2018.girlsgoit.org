2e80b63c4942d7d350934c585e9bc76d051e51a8
ciobanucristina <daniela.ciobanu.2000@mail.ru>
2018-07-17 17:09:35 +0300

    Define model Note and register the model in admin

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Note(models.Model):
+    created_date = models.DateField(auto_now=True)
+    modified_date = models.DateField()
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'Created date: {self.created_date}, modified date: {self.modified_date}, user: {self.user}.'
