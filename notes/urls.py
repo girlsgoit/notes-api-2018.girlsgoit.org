f8c2bb9f02a673a52a8b715f9001ecbccdc19be0
InagoesIT <wenulife@gmail.com>
2018-07-17 17:50:42 +0300

    Detail list view and url for note model

--- notes/urls.py
+++ notes/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from .views import note_list
 
 urlpatterns = [
+    path('notes/', note_list, name='note_list')
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Note
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import NoteSerializer
 
-# Create your views here.
+@api_view (['GET','POST'])
+def note_list(request):
+    if request.method == 'GET':
+        notes = Note.objects.all()
+        note_serializer = NoteSerializer(notes, many=True)
+        return Response(note_serializer.data)
+    elif request.method == 'POST':
+        note_data = request.data
+        note_serializer = NoteSerializer (data=note_data)
+        if note_serializer.is_valid():
+            note_serializer.save()
+            return Response(note_serializer.data)
+        else:
+            return Response(note_serializer.error, status=400)
+
+
+
+
