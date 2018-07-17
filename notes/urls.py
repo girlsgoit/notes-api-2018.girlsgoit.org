47d171ef17a4fbf6b73122cceccfd8b7cddb3fdb
InagoesIT <wenulife@gmail.com>
2018-07-17 17:55:10 +0300

    fix missing comma

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from .views import note_list
 from .views import note_detail
 
 urlpatterns = [
-    path('notes/', note_list, name='note_list')
+    path('notes/', note_list, name='note_list'),
     path('notes/<int:note_id>', note_detail, name='note-detail')
 ]
