a9ffe1fe8bc5a8098440216f4904b43a383a4a7a
Marc Graciov <mark.graciov@gmail.com>
2018-07-19 06:57:13 +0300

    [backend] do something good

--- notes_api/settings/production.py
+++ notes_api/settings/production.py
@@ -6,6 +6,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['notes.girlsgoit.org', 'notes-api.girlsgoit.org', 'ggit-notes-api.azurewebsites.net']
 CORS_ORIGIN_WHITELIST = ('notes.girlsgoit.org', 'notes-api.girlsgoit.org', 'ggit-notes-api.azurewebsites.net')
+CSRF_TRUSTED_ORIGINS = ['notes.girlsgoit.org', 'notes-api.girlsgoit.org']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
